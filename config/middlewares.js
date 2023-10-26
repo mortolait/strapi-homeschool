2b8300faeda609a661fa42ef6a67ca76e8ab5441
mortolait <contatomortola@outlook.com>
2023-10-26 07:47:28 -0300

    ajuste de segurança

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,27 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = {
+  load: {
+    before: [
+      'strapi::errors',
+      'strapi::security',
+      'strapi::cors',
+      'strapi::poweredBy',
+      'strapi::logger',
+      'strapi::query',
+      'strapi::body',
+      'strapi::session',
+      'strapi::favicon',
+      'strapi::public',
+    ],
+  },
+  settings: {
+    security: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          connectSrc: ["'self'", "http://172.173.237.220:1337"],
+          // ... outras diretivas, se necessário ...
+        },
+      },
+    },
+  },
+};
